421eb833f5686c587a9fc8465c6384d65a96bdef
İrem <iremcrnblt@gmail.com>
2023-01-10 00:29:33 +0300

    ViewModel Text Editor

--- Gunluk/Data/Gonderi.cs
+++ Gunluk/Data/Gonderi.cs
@@ -6,15 +6,20 @@ namespace Gunluk.Data
     {
         public int Id { get; set; }
 
-        [Required]
+        [Display(Name ="Başlık")]
+        [Required(ErrorMessage ="{0} alanı zorunludur.")]
         [MaxLength(400)]
         public string Baslik { get; set; } = null!;
+
+        [Display(Name = "İçerik")]
         public string? Icerik { get; set; } = "";
 
         [MaxLength(255)]
         public string? Resim { get; set; }
         public DateTime OlusturulmaZamani { get; set; } =DateTime.Now;
         public DateTime DegistirilmeZamani { get; set; } = DateTime.Now;
+
+        [Display(Name = "Kategori")]
         public int KategoriId { get; set; }
 
         //navigation property
